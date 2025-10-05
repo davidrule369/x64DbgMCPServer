08b6691a17d9367805f39ab46cd6f6878affea82
davidrule369 <david.rule369@slmails.com>
2025-10-05 06:25:19 +0530

    Update Attributes.DllExport.cs

--- DotNetPlugin.Stub/Attributes.DllExport.cs
+++ DotNetPlugin.Stub/Attributes.DllExport.cs
@@ -1,27 +1,20 @@
 namespace RGiesecke.DllExport
 {
-	using System;
-	using System.Runtime.InteropServices;
+    using System;
+    using System.Runtime.InteropServices;
 
-	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-	public sealed class DllExportAttribute : Attribute
-	{
-		public DllExportAttribute()
-		{
-		}
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class DllExportAttribute : Attribute
+    {
+        public DllExportAttribute() { }
+        public DllExportAttribute(string entryPoint) { EntryPoint = entryPoint; }
+        public DllExportAttribute(string entryPoint, CallingConvention callingConvention)
+        {
+            EntryPoint = entryPoint;
+            CallingConvention = callingConvention;
+        }
 
-		public DllExportAttribute(string entryPoint)
-		{
-			this.EntryPoint = entryPoint;
-		}
-
-		public DllExportAttribute(string entryPoint, CallingConvention callingConvention)
-		{
-			this.EntryPoint = entryPoint;
-			this.CallingConvention = callingConvention;
-		}
-
-		public string EntryPoint { get; }
-		public CallingConvention CallingConvention { get; }
-	}
+        public string EntryPoint { get; }
+        public CallingConvention CallingConvention { get; }
+    }
 }
