dc86ff2dce82021cff888a546e834158158155f7
David Rule <david.rule369@slmails.com>
2025-10-08 03:40:34 +0530

    Add files via upload

--- DotNetPlugin.Impl/Properties/Resources.Designer.cs
+++ DotNetPlugin.Impl/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace DotNetPlugin.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MainIcon {
+            get {
+                object obj = ResourceManager.GetObject("MainIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Debugger Command Help Manual
         ///
         ///InitDebug
